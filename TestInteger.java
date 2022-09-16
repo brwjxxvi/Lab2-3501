e3412e3a7e28e243d2aaeffd162a322e18386535
yubo <mao00071@morris.umn.edu>
2022-09-15 23:47:40 -0500

    increasing order and decreasing order

--- TestInteger.java
+++ TestInteger.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TestInteger implements Comparable<TestInteger> {
 
@@ -58,6 +59,13 @@ public class TestInteger implements Comparable<TestInteger> {
         Arrays.sort(MTI);
         //System.out.println(Arrays.toString(MTI));
         System.out.println("Timsort: " + counter);
+
+        TestInteger[] IncreaseTI = new TestInteger[100]; 
+        for(int i = 0; i<IncreaseTI.length; i++){
+            IncreaseTI[i] = new TestInteger(ThreadLocalRandom.current().nextInt (1, 102));
+        }
+        System.out.println(Arrays.toString(IncreaseTI));
+            
         
     }
     
